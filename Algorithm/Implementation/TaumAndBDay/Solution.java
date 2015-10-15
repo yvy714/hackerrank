fe3f4fd89bba935d1ef5d6a0f4b7076305ae7edd
Yuan Yao <yuanyao@pc-203-205.customer.nott.opal.lan>
2015-10-15 20:54:43 +0100

    Taum and B'day
    solution to Taum and B’day

---
+++ Algorithm/Implementation/TaumAndBDay/Solution.java
@@ -0,0 +1,47 @@
+/***
+Problem description:
+Taum is planning to celebrate the birthday of his friend, Diksha. There are two types of gifts that Diksha wants from Taum: one is black and the other is white. To make her happy, Taum has to buy B number of black gifts and W number of white gifts.
+
+The cost of each black gift is X units.
+The cost of every white gift is Y units.
+The cost of converting each black gift into white gift or vice versa is Z units.
+Help Taum by deducing the minimum amount he needs to spend on Diksha's gifts.
+
+Input Format
+
+The first line will contain an integer T which will be the number of test cases.
+There will be T pairs of lines. The first line of each test case will contain the values of integers B and W. Another line of each test case will contain the values of integers X, Y, and Z.
+
+Constraints 
+1<=T<=10 
+0<=X,Y,Z,B,W<=10^9
+Output Format
+
+T lines, each containing an integer: the minimum amount of units Taum needs to spend on gifts.
+ ***/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int i = 0; i < t; i++){
+            long b = in.nextLong();
+            long w = in.nextLong();
+            long x = in.nextLong();
+            long y = in.nextLong();
+            long z = in.nextLong();
+            
+            if(y+z < x) x = y+z;
+            if(x+z < y) y = x+z;
+            System.out.println(b*x + w*y);
+        }
+    }
+}
